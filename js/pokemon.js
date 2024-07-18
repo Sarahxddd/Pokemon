05db885ebd7be9f34f7b2eeed563586d1ff20407
Sarahxddd <sarahimendizabalj@gmail.com>
2024-07-18 10:09:41 -0600

    Ataque y mascota enemigo

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,4 +1,5 @@
 let ataqueJugador
+let ataqueEnemigo
 
 function iniciarJuego(){
     let botonMascotaJugador = document.getElementById("boton-pokemon")
@@ -21,8 +22,8 @@ function iniciarJuego(){
 
 function seleccionarMascotaJugador(){
 
-    let seleccionarMascota = document.getElementById("boton-pokemon")
-    seleccionarMascota.style.display="none"
+    let seccionSeleccionarMascota = document.getElementById("boton-pokemon")
+    seccionSeleccionarMascota.style.display="none"
 
     let seccionSeleccionAtaque = document.getElementById("seleccionar-ataque")
     seccionSeleccionarAtaque.style.display="flex"
@@ -32,6 +33,8 @@ function seleccionarMascotaJugador(){
     let imputBoulbasour = document.getElementById("boulbasour")
 
     let imputSquirtle = document.getElementById("squirtle")
+
+    let spanMascota = document.getElementById("mascota-jugador")
 
     if (imputCharmander.checked){
 
@@ -55,18 +58,64 @@ function seleccionarMascotaJugador(){
 
 }
 
+function seleccionarMascotaEnemigo(){
+    
+    let ataqueAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+
+    if(ataqueAleatorio == 1){
+
+        spanMascotaEnemigo.innerhtml = "charmander"
+
+    } else if(ataqueAleatorio == 2){
+
+        spanMascotaEnemigo.innerhtml = "boulbasour"
+
+    } else{
+
+        spanMascotaEnemigo.innerhtml = "squirtle"
+    }
+}
+
 function ataqueFuego(){
 
     ataqueJugador = "fuego"
+    ataqueAleatorioEnemigo()
     
 }
 
 function ataqueAgua(){
 
     ataqueJugador = "agua"
+    ataqueAleatorioEnemigo()
 }
 
 function ataqueTierra(){
 
     ataqueJugador = "tierra"
+    ataqueAleatorioEnemigo()
 }
+
+
+function ataqueAleatorioEnemigo(){
+
+    let ataqueAleatorio = aleatorio(1,3)
+    if(ataqueAleatorio == 1){
+
+        ataqueEnemigo = "fuego"
+
+    }else if (ataqueAleatorio == 2){
+
+        ataqueEnemigo = "agua"
+    } else{
+
+        ataqueEnemigo = "tierra"
+    }
+}
+
+function Aleatorio(min,max){
+
+    return Math.floor(math.random() * (max - min + 1) + min)
+}
+
+window.addEventListener("load", iniciarJuego)
